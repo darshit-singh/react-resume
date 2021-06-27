a46f15aad456f209e9f23a48110558d211f4750a
Darshit Sarna <darshit.sarna@pristyncare.com>
2021-06-27 13:14:11 +0530

    basic file structure for the resume done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+import Testimonials from './Components/Testimonials';
 
 function App() {
   return (
     <div className="App">
       hi
+      <Header />
+      <About />
+      <Contact />
+      <Footer />
+      <Portfolio />
+      <Resume />
+      <Testimonials />
     </div>
   );
 }
