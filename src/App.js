966a2b97084cdb33bd72abd4f95b1606e1d3b4bf
Darshit Sarna <darshit.sarna@pristyncare.com>
2021-06-28 00:37:36 +0530

    initial template built

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
-import Portfolio from './Components/Portfolio';
-import Resume from './Components/Resume';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <About />
-      <Resume />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-export default function Footer() {
+class Footer extends Component {
+  render() {
     return (
-        <div>
-            Footer
-        </div>
-    )
+      <footer>
+
+      <div className="row">
+
+         <div className="twelve columns">
+
+
+            <ul className="copyright">
+               <li>&copy; Copyright 2014 CeeVee</li>
+               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            </ul>
+
+         </div>
+
+         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+
+      </div>
+
+   </footer>
+    );
+  }
 }
+
+export default Footer;
