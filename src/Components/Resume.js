2a2e565dc47642cc4bc519a3827c5668103a9f46
Darshit Sarna <darshit.sarna@pristyncare.com>
2021-09-27 17:17:16 +0530

    redid the skills section to make it respond to the json

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,32 +3,35 @@ import React, { Component } from 'react';
 class Resume extends Component {
     render() {
         if (this.props.data) {
-            
+
             var education = this.props.data.education.map((edu) => {
-                return (<div key = {edu.school} className="row item">
-                            <div className="twelve columns">
-                                <h3>{edu.school}</h3>
-                                <p className="info">{edu.degree}<span>&bull;</span> <em className="date">{edu.graduated}</em></p>
-                                <p>
-                                    {edu.description}
-                                </p>
-                            </div>
-                        </div>)
+                return (<div key={edu.school} className="row item">
+                    <div className="twelve columns">
+                        <h3>{edu.school}</h3>
+                        <p className="info">{edu.degree}<span>&bull;</span> <em className="date">{edu.graduated}</em></p>
+                        <p>
+                            {edu.description}
+                        </p>
+                    </div>
+                </div>)
             })
             var work = this.props.data.work.map((job) => {
-                return (<div key = {job.company} className="row item">
-                            <div className="twelve columns">
-                                <h3>{job.company}</h3>
-                                <p className="info">{job.title}<span>&bull;</span> <em className="date">{job.years}</em></p>
+                return (<div key={job.company} className="row item">
+                    <div className="twelve columns">
+                        <h3>{job.company}</h3>
+                        <p className="info">{job.title}<span>&bull;</span> <em className="date">{job.years}</em></p>
 
-                                <p>
-                                    {job.description}
-                                </p>
-                            </div>
-                        </div>)
+                        <p>
+                            {job.description}
+                        </p>
+                    </div>
+                </div>)
             })
 
-            var skills = this.props.data.skills
+            var skills = this.props.data.skills.map((skill) => {
+                let className = 'bar-expand ' + skill.name.split(' ').join('').toLowerCase()
+                return <li key={skill.name}><span style={{ width: skill.level }} className={className}></span><em>{skill.name}</em></li>
+            })
             var skillsDescription = this.props.data.skillsDescription
         }
         return (
@@ -48,24 +51,27 @@ class Resume extends Component {
                     </div>
 
                     <div className="nine columns main-col">
-                    {education}
+                        {education}
                     </div>
                 </div>
                 <div className="row skill">
                     <div className="three columns header-col">
                         <h1><span>Skills</span></h1>
+                        {/* <p className="info">{edu.degree}<span>&bull;</span> <em className="date">{edu.graduated}</em></p> */}
                     </div>
                     <div className="nine columns main-col">
-                        {/* <p>{skillsDescription}</p> */}
+                        <p style={{ margin: '7px 0 50px 0' }}>{skillsDescription} :</p>
+
                         <div className="bars">
-                            {skills && <ul className="skills">
+                            {/* {skills && <ul className="skills">
                                 <li><span className="bar-expand photoshop"></span><em>{skills[0].name}</em></li>
                                 <li><span className="bar-expand illustrator"></span><em>{skills[1].name}</em></li>
                                 <li><span className="bar-expand wordpress"></span><em>{skills[2].name}</em></li>
                                 <li><span className="bar-expand css"></span><em>{skills[3].name}</em></li>
-                                {/* <li><span className="bar-expand html5"></span><em>{skills[4].name}</em></li> */}
+                                <li><span className="bar-expand html5"></span><em>{skills[4].name}</em></li>
                                 <li><span className="bar-expand jquery"></span><em>{skills[5].name}</em></li>
-                            </ul>}
+                            </ul>} */}
+                            {skills}
                         </div>
                     </div>
                 </div>
